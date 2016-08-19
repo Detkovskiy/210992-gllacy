5d4a2a6277b29bf9bdebf3199e33ae7e960b86e4
Yura <9119113142@MAIL.RU>
2016-08-19 16:54:09 +0400

    подгон 1 страницы

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ function init(){
     var myMap;
  
     myMap = new ymaps.Map("map", {
-        center: [59.9394, 30.3298],
+        center: [59.9393, 30.3292],
         zoom: 16,
         controls: []
     });
@@ -14,8 +14,8 @@ function init(){
      
     var myPlacemark = new ymaps.Placemark([59.9386, 30.3230] , {},
         { iconLayout: 'default#image',
-          iconImageHref: 'img/icon/pin.svg',
-          iconImageSize: [80, 146],
+          iconImageHref: 'img/pin.png',
+          iconImageSize: [230, 146],
           iconImageOffset: [-45, -147] });     
  
     myMap.geoObjects.add(myPlacemark);
